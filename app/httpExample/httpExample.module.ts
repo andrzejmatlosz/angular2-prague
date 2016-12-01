cf56edd8c2bde82977174948d68c425f53a838ae
Matłosz <ajms@gft.com>
2016-12-01 17:19:49 +0100

    finish example with http for all options

--- app/httpExample/httpExample.module.ts
+++ app/httpExample/httpExample.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HttpModule }    from '@angular/http';
 import { HttpExampleComponent } from './httpExample.component';
 import { PersonObservableService } from './personObservable.service';
+import { PersonPromiseService } from './personPromise.service';
 
 @NgModule({
     imports: [ HttpModule, CommonModule ],
     declarations: [ HttpExampleComponent ],
     exports: [ HttpExampleComponent ],
-    providers: [ PersonObservableService ]
+    providers: [ PersonObservableService, PersonPromiseService ]
 })
 export class HttpExampleModule { }
